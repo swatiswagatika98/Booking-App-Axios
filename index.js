6fefa775151db2266e27be024f787feb2c35a8e9
swatiswagatika98 <swatiswagatika.1998mishra@gmail.com>
2024-01-25 06:31:30 -0800

    saving the user details on crud crud

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-const axiosInstance = axios.create({
-    baseURL : 'https://crudcrud.com/api/74a2572894ad4d70859eff1648642425'
-    baseURL : 'https://crudcrud.com/api/96374143b6a94b12b762146cecc019f9'
-});
 function handleFormSubmit(event) {
     event.preventDefault();
-@@ -58,11 +58,28 @@ function showNewUser(user){
-            document.getElementById('email').value = res.data.email;
-            document.getElementById('phone').value = res.data.phone; 
-        });
-        axiosInstance.delete(`/bookAppointment/${user._id}`)
-        .then();
-        parent.removeChild(li);
-        document.getElementById('submit').style.display = 'none';
-        document.getElementById('edit').style.display = 'inline-block';
+    const name = event.target.username.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
 
-        document.getElementById('edit').onclick = () => {
-            const name = document.getElementById('username').value;
-            const email = document.getElementById('email').value;
-            const phone = document.getElementById('phone').value;
-
-            const obj = {
-                name,
-                email,
-                phone
-            };
-
-            axiosInstance.put(`/bookAppointment/${user._id}`, obj)
-                .then((res) => {
-                    location.reload();
-                });
-        }
+    const obj = {
+        name,
+        email,
+        phone
     }
-    li.appendChild(input);
-    li.appendChild(edit);
-    parent.appendChild(li);
-}
+
+    axios.post('https://crudcrud.com/api/a004d8ad7b6a4d528ea1f1e31b685429/bookAppointment', obj)
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
 }
